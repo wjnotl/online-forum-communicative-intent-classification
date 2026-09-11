d733271e7ad60931dcd3167c9fa668a5a3dc81d6
wjnotl <liewwenjing34882@gmail.com>
2026-09-11 18:44:49 +0800

    remove reindex

--- reindex.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("dataset.csv")
-df = df[~df['id'].isin([74146])]
-df = df.reset_index(drop=True)
-df['id'] = df.index + 1
-df.to_csv("dataset_2.csv", index=False)
